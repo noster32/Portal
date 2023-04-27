47eea3e19cd6212e37ab454d9601e6ac411d2304
noster32 <120433537+noster32@users.noreply.github.com>
2023-04-28 04:00:56 +0900

    .

---
+++ Assets/Scripts/Portal/CPlayerCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayerCamera : CComponent
+{
+    #region public
+
+    public float sensitivity;
+    public Transform orientation;
+
+    #endregion
+
+    float xRotation;
+    float yRotation;
+
+
+    public override void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public override void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
