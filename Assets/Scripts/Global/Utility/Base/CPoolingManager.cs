b5a4e6ff1761dc4ee9fd18b2dcda02742c300bf2
noster32 <120433537+noster32@users.noreply.github.com>
2023-09-29 15:59:29 +0900

    .

--- Assets/Scripts/Global/Utility/Base/CPoolingManager.cs
+++ Assets/Scripts/Global/Utility/Base/CPoolingManager.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public abstract class CPoolingManager<T> : CComponent where T : CComponent 
 {
-    private T prefeb;
+    private T prefab;
     private ObjectPool<T> pool;
 
     private ObjectPool<T> Pool
@@ -20,4 +22,23 @@ public abstract class CPoolingManager<T> : CComponent where T : CComponent
 
         set => pool = value;
     }
+
+    protected void InitPool(T initPrefab, int initial = 10, int max = 20, bool collectionChecks = false)
+    {
+        prefab = initPrefab;
+        pool = new ObjectPool<T>
+        {
+
+        }
+    
+    }
+
+    protected virtual T CreateSetup() => Instantiate(prefab);
+    protected virtual void GetSetup(T obj) => obj.gameObject.SetActive(true);
+    protected virtual void ReleaseSetup(T obj) => obj.gameObject.SetActive(false);
+    protected virtual void DestroySetup(T obj) => Destroy(obj);
+
+    public T Get() => Pool.Get();
+    public void Release(T obj) => Pool.Release(obj);
+
 }
